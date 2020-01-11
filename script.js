bf8235fb23fe9f4dca2f807b92ac23d06af5f3ba
nionata <n.ionata129@gmail.com>
2020-01-10 19:08:05 -0500

    Removed acc

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const request = require('request').defaults({
 
 const baseUrl = 'https://auth.impulse.com:8443'
 const form = {
-  userId: "nionata",
-  pass: "WeAreReallyOutHereBaby",
+  userId: "big",
+  pass: "penis",
   platform: "MacIntel",
   appversion: "5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Mobile Safari/537.36"
 }
@@ -22,10 +22,10 @@ const formReq = {
 const doReq = function() {
   const date = new Date()
   console.log(`${date.getHours()}:${date.getMinutes()} checking authentication...`)
-  
+
   request(baseUrl + '/clientStatus.!%5E', function (err, response, body) {
     if(err) console.log(err)
-    
+
     if (body) {
       const noAuthed = body.includes('UF SafeConnect')
 
@@ -52,6 +52,6 @@ const doReq = function() {
 
 
 doReq() // Check auth on start
-setInterval(function () { // Every 15 min, auth
+setInterval(function () { // Every 1 min, auth
   doReq()
 }, 1000*60*1)
